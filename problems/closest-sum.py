225702011596ccd954f995c69080fa0ee3693eb4
jsingh <jitsingh@redhat.com>
2021-08-15 08:41:31 +0530

    more problems

---
+++ problems/closest-sum.py
@@ -0,0 +1,39 @@
+def find_closest_sum(A, target):
+    min_diff = float("inf")
+    low = 0
+    high = len(A) - 1
+    closest_num = None
+
+    if len(A) == 0:
+        return None
+    if len(A) == 1:
+        return A[0]
+
+    while(low <= high):
+        mid = (low + high) // 2
+
+        if mid+1 < len(A):
+            min_diff_right = abs(A[mid+1] - target)
+        if mid > 0:
+            min_diff_left = abs(A[mid-1] - target)
+        
+
+        if min_diff_left < min_diff:
+            min_diff = min_diff_left
+            closest_num = A[mid-1]
+        if min_diff_right < min_diff:
+            min_diff = min_diff_right
+            closest_num = A[mid+1]
+        
+        if A[mid] < target:
+            low = mid + 1
+        elif A[mid] > target:
+            high = mid - 1
+        else:
+            return A[mid]
+    return closest_num
+
+A = [2,5,6,7,8,8,9]
+t = -1
+y = find_closest_sum(A,t)
+print(y)
--- problems/longest-palin-str.py
+++ problems/longest-palin-str.py
@@ -1,3 +1,46 @@
+from collections import Counter
 class Solution:
+    def __init__(self) -> None:
+        self.palindrome = {}
+        self.loopflag = 0
     def longestPalindrome(self, s: str) -> str:
-        pass
+        end = len(s)
+        i = 0
+        new_str = ""
+        res = ""
+        while(i < end):
+            for ch in range(end):
+                new_str = new_str+s[ch]
+                print(new_str)
+            flag = self.checkPalindrome(new_str)
+            if flag:
+                self.store(new_str)
+            i+=1
+            if i >= end:
+                self.loopflag+=1
+                i = self.loopflag
+                ch = i
+            new_str=""
+        for substr in self.palindrome.keys():
+            max = 1
+            if self.palindrome[substr] >= max:
+                res = substr
+                max = self.palindrome[substr]
+        return res
+
+        
+    def checkPalindrome(self, new_str: str) -> bool:
+        if new_str == new_str[::-1]:
+            return True
+        else:
+            return False
+
+    def store(self, new_str: str)-> dict:
+        lenght = len(new_str)
+        self.palindrome[new_str] = lenght
+        # print(self.palindrome)
+            
+
+ss = Solution()
+output = ss.longestPalindrome("cbbd")
+# print(output)
