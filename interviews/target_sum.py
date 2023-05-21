9a9807364a1370042c2b71c2491b494d44d76254
Jitendar Singh <jitsingh@redhat.com>
2023-05-21 20:34:13 +0530

    more

--- interviews/target_sum.py
+++ interviews/target_sum.py
@@ -1,2 +1,11 @@
 # Given a list of integers, write a Python function to find the two numbers that add up to a given target sum.
 
+def target_sum(nums, target):
+    for num in nums:
+        if (target - num) in nums:
+            return num, target-num
+
+numbers = [2,9, 4, 7, 11, 15]
+target = 11
+result = target_sum(numbers,target)
+print(result)
