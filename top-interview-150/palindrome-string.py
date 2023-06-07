e6224a5ef6dbd6687f6109e13fb0b762f6a5fb92
Jitendar Singh <jitsingh@redhat.com>
2023-06-07 15:23:04 +0530

    Todays work

--- top-interview-150/palindrome-string.py
+++ top-interview-150/palindrome-string.py
@@ -2,9 +2,6 @@ def is_palindrome(s):
     reverse_s = ''.join(char.lower() for char in s if char.isalnum())
     return s == s[::-1]
 
-
-
-
 print(is_palindrome("radar"))         # Output: True
 print(is_palindrome("python"))        # Output: False
 print(is_palindrome("A man, a plan"))  # Output: True
